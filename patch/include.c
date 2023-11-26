5460ff6397303200edeefad99f193c4796a01da5
Dronne34 <ozy23as@gmail.com>
2023-11-26 13:51:22 +0200

    dwm-flx-6.4 no change

---
+++ patch/include.c
@@ -0,0 +1,34 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_alpha.c"
+#include "bar_dwmblocks.c"
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_statuscmd.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+
+/* Other patches */
+#include "cyclelayouts.c"
+#include "focusfollowmouse.c"
+#include "focusmaster.c"
+#include "focusurgent.c"
+#include "fullscreen.c"
+#include "pertag.c"
+#include "restartsig.c"
+#include "rotatestack.c"
+#include "togglefullscreen.c"
+#include "vanitygaps.c"
+#include "xrdb.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_deck.c"
+#include "layout_fibonacci.c"
+#include "layout_gapplessgrid.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
